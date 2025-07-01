049f8df2ccc90534bff3b99c1c68406d061310b5
SoulPixelIV <william.dj@outlook.de>
2025-07-01 22:07:52 +0200

    Cards can now be discarded so that new cards can be drawn from the deck

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,5 +1,8 @@
 extends TextureButton
 
+var handPos = 0
+var card_ui_parent = null
+
 func _ready() -> void:
 	pivot_offset = size / 2
 
@@ -8,3 +11,7 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	scale = Vector2(1, 1)
+
+func _on_button_down() -> void:
+	card_ui_parent.cardSlots[handPos] = 0
+	queue_free()
--- Scripts/card_ui.gd
+++ Scripts/card_ui.gd
@@ -1,27 +1,37 @@
 extends Control
 
-@export var numberOfCards = 0 
-@export var maxNumberOfCards = 5
 @export var cardTimerSpeed = 30
 
-var card = preload("res://Scenes/card.tscn") 
+var card = preload("res://Scenes/card.tscn")
+var maxNumberOfCards = 5
+var cardSlots = [0, 0, 0, 0, 0]
+var currCardNum = 0
 @onready var card_container = $CardContainer
 @onready var timer_bar = $TimerBar
 
 func _ready() -> void:
-	timer_bar.value = 0
+	timer_bar.value = 100
 	
 func _process(delta: float) -> void:
 	if timer_bar.value < 100:
 		timer_bar.value += delta * cardTimerSpeed
 
-func initiateCard() -> void: 
-	numberOfCards += 1 
-	timer_bar.value = 0
-	var spawnedCard = card.instantiate() 
-	spawnedCard.position = Vector2(30 + 40 * numberOfCards, 150) 
-	card_container.add_child(spawnedCard) 
+func initiateCard() -> void:
+	currCardNum = 0
+	for i in range(cardSlots.size()):
+		if cardSlots[i] == 0:
+			cardSlots[i] = 1
+			break
+		currCardNum += 1
+		
+	if currCardNum < 5:	
+		timer_bar.value = 0
+		var spawnedCard = card.instantiate()
+		spawnedCard.card_ui_parent = self
+		spawnedCard.handPos = currCardNum
+		spawnedCard.position = Vector2(60 + 40 * currCardNum, 150)
+		card_container.add_child(spawnedCard) 
 
 func _on_draw_card_button_pressed() -> void:
-	if numberOfCards < maxNumberOfCards and timer_bar.value == 100.0: 
+	if timer_bar.value == 100.0: 
 		initiateCard()
