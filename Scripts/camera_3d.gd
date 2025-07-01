7484a0e8bb214f35fcf9fbba5eaf6828c4360696
SoulPixelIV <william.dj@outlook.de>
2025-07-01 17:43:16 +0200

    Added functioning draw card button

--- Scripts/camera_3d.gd
+++ Scripts/camera_3d.gd
@@ -3,11 +3,6 @@ extends Camera3D
 @export var mov_speed: float = 5.0
 
 var card_ui = preload("res://Scenes/card_ui.tscn")
-var card = preload("res://Scenes/card.tscn")
-
-func initiateCard() -> void:
-	var spawnedCard = card.can_instantiate()
-	add_child(spawnedCard)
 
 func _ready() -> void:
 	var ui = card_ui.instantiate()
--- Scripts/draw_card_button.gd
+++ Scripts/draw_card_button.gd
@@ -1,5 +1,16 @@
 extends Button
 
+var numberOfCards = 0
+var maxNumberOfCards = 5
+var card = preload("res://Scenes/card.tscn")
+@onready var card_container = get_parent().get_node("CardContainer")
+
+func initiateCard() -> void:
+	if numberOfCards < maxNumberOfCards:
+		numberOfCards += 1
+		var spawnedCard = card.instantiate()
+		spawnedCard.position = Vector2(30 + 40 * numberOfCards, 150)
+		card_container.add_child(spawnedCard)
+	
 func _on_pressed() -> void:
-	var currCamera = get_node("res://Scenes/camera_3d.tscn")
-	currCamera.initiateCard()
+	initiateCard()
