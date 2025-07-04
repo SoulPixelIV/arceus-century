862a48b44dea818134c6ad686a4e8eb79bca2c23
SoulPixelIV <william.dj@outlook.de>
2025-07-04 16:02:34 +0200

    Replaced card arrow with selected floating card on screen

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,18 +1,42 @@
 extends TextureButton
 
 var handPos = 0
+var original_posX = 0
+var original_posY = 0
 var card_ui_parent = null
+var curr_hovered = false
+var card_is_selected = false
 
 func _ready() -> void:
 	pivot_offset = size / 2
 
 func _on_mouse_entered() -> void:
-	scale = Vector2(1.2, 1.2)
+	if !card_is_selected:
+		curr_hovered = true
+		z_index = 100
+		scale = Vector2(1.2, 1.2)
+		position += Vector2(0, -20)
 
 func _on_mouse_exited() -> void:
-	scale = Vector2(1, 1)
+	if !card_is_selected:
+		curr_hovered = false
+		z_index = 0
+		scale = Vector2(1, 1)
+		position -= Vector2(0, -20)
 
 func _on_button_down() -> void:
-	if !card_ui_parent.card_selected:
-		card_ui_parent.card_selected = true
-		card_ui_parent.arrow_start_pos = position
+	if curr_hovered and !card_ui_parent.card_is_selected:
+		card_ui_parent.card_is_selected = true
+		card_has_focus()
+	elif card_is_selected:
+		card_ui_parent.card_is_selected = false
+		z_index = 0
+		scale = Vector2(1, 1)
+		position = Vector2(original_posX, original_posY - 20)
+		card_is_selected = false
+		
+func card_has_focus() -> void:
+	card_is_selected = true
+	z_index = 100
+	scale = Vector2(1.2, 1.2)
+	position = Vector2(370, 170)
--- Scripts/card_ui.gd
+++ Scripts/card_ui.gd
@@ -3,14 +3,10 @@ extends Control
 @export var cardTimerSpeed = 30
 
 var card = preload("res://Scenes/card.tscn")
-var arrowTexture = preload("res://Textures/arrow.png")
 var maxNumberOfCards = 5
 var cardSlots = [0, 0, 0, 0, 0]
 var currCardNum = 0
-var arrow = null
-var arrow_start_pos = Vector2(0, 0)
-var card_selected = false
-var arrow_spawned = false
+var card_is_selected = false
 
 @onready var card_container = $CardContainer
 @onready var timer_bar = $TimerBar
@@ -18,38 +14,7 @@ var arrow_spawned = false
 func _ready() -> void:
 	timer_bar.value = 100
 	
-func _process(delta: float) -> void:
-	#Card Arrow
-	if card_selected:
-		if !arrow_spawned:
-			arrow = TextureRect.new()
-			arrow.texture = arrowTexture
-			arrow.position = Vector2(100, 100)
-			add_child(arrow)
-			arrow_spawned = true
-	else:
-		arrow_spawned = false
-		
-	if arrow != null:
-		var from_pos = arrow_start_pos  # Position, z. B. von der Karte
-		var to_pos = get_viewport().get_mouse_position()
-		var arrow_direction = to_pos - from_pos
-		var arrow_distance = arrow_direction.length()
-		var arrow_angle = arrow_direction.angle()
-
-		arrow.global_position = from_pos
-		arrow.rotation = arrow_angle
-		var base_width = arrow.texture.get_width()
-		arrow.pivot_offset = Vector2(0, arrowTexture.get_height() / 2.0)
-		arrow.stretch_mode = TextureRect.STRETCH_SCALE
-		arrow.scale.x = arrow_distance / base_width
-		
-	#Release Arrow
-	if Input.is_action_just_pressed("exit"):
-		if card_selected:
-			arrow.queue_free()
-			card_selected = false
-		
+func _process(delta: float) -> void:		
 	#Timer Bar
 	if timer_bar.value < 100:
 		timer_bar.value += delta * cardTimerSpeed
@@ -68,6 +33,8 @@ func initiateCard() -> void:
 		spawnedCard.card_ui_parent = self
 		spawnedCard.handPos = currCardNum
 		spawnedCard.position = Vector2(100 + 60 * currCardNum, 280)
+		spawnedCard.original_posX = 100 + 60 * currCardNum
+		spawnedCard.original_posY = 280
 		card_container.add_child(spawnedCard) 
 
 func _on_draw_card_button_pressed() -> void:
