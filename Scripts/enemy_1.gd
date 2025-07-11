ce7b6116e8c424edb7803f74ac2dd856d500156f
SoulPixelIV <william.dj@outlook.de>
2025-07-11 14:58:16 +0200

    Enemy health is now visible on screen

--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -1,6 +1,23 @@
 extends StaticBody3D
 
 var display_name = "Holy Diver"
+var health = 14
+
+var labels = []
+var enemy_health_text = null
+var enemy_health_label = null
+
+func _ready() -> void:
+	labels = get_tree().get_nodes_in_group("ui_labels")
+	for label in labels:
+		if label.name == "EnemyHealthText":
+			enemy_health_text = label
+		if label.name == "EnemyHealthLabel":
+			enemy_health_label = label
+	
+func _process(delta: float) -> void:
+	enemy_health_text.text = "En. Health:"
+	enemy_health_label.text = str(health)
 
 func on_hover():
 	scale = Vector3(1.2, 1.2, 1.2)
