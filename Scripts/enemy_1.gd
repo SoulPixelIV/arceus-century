a6ae9b36c76a71f9d2f1c61dab5c4a501aacdc46
SoulPixelIV <william.dj@outlook.de>
2025-07-14 16:55:39 +0200

    Enemies can now block attacks

--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -4,14 +4,19 @@ var display_name = "Holy Diver"
 var health = 14
 var damage = 6
 var strength_buff = 0
-var attack_speed = 6
+var attack_speed = 8
+var block_strength = 6
+var block_speed = 12
 
 var element_water = false
 var keylock = 0
 
 var attack_timer = 0
 var idle_delay = 1
+
 var idle_delay_save = idle_delay
+var block_length = 0
+var block_strength_save = block_strength
 
 enum States {IDLE, ATTACKING, BLOCKING}
 var state: States = States.IDLE
@@ -21,9 +26,14 @@ var labels2 = []
 var labels3 = []
 var enemy_health_text = null
 var enemy_health_label = null
+var block_text = null
+var block_label = null
 var enemy_attack_bar = null
 var player_node = null
 var card_ui_node = null
+
+var shield_texture: Texture2D = preload("res://Textures/shield_icon_small.png")
+var shield_sprite: Sprite3D
 
 func _ready() -> void:
 	labels = get_tree().get_nodes_in_group("ui_labels")
@@ -34,6 +44,10 @@ func _ready() -> void:
 			enemy_health_label = label
 		if label.name == "EnemyAttackBar":
 			enemy_attack_bar = label
+		if label.name == "BlockText":
+			block_text = label
+		if label.name == "BlockLabel":
+			block_label = label
 			
 	labels2 = get_tree().get_nodes_in_group("player")
 	for label2 in labels2:
@@ -44,11 +58,23 @@ func _ready() -> void:
 	for label3 in labels3:
 		if label3.name == "Card UI":
 			card_ui_node = label3
+			
+	#Create Shield Icon
+	shield_sprite = Sprite3D.new()
+	shield_sprite.texture = shield_texture
+	shield_sprite.position = Vector3(0, 0, 0.1)
+	shield_sprite.visible = false
+	add_child(shield_sprite)
 	
 func _process(delta: float) -> void:
 	enemy_health_text.text = "En. Health:"
 	enemy_health_label.text = str(health)
-	enemy_attack_bar.value = attack_timer
+	if state == States.IDLE:
+		enemy_attack_bar.value = 0
+	if state == States.BLOCKING:
+		enemy_attack_bar.value = block_length
+	if state == States.ATTACKING:
+		enemy_attack_bar.value = attack_timer	
 	
 	#Idling
 	if state == States.IDLE:
@@ -68,7 +94,17 @@ func _process(delta: float) -> void:
 			
 	#BLOCKING
 	if state == States.BLOCKING:
-		state = States.IDLE
+		shield_sprite.visible = true
+		block_text.text = "Block:"
+		block_label.text = str(block_strength)
+		block_length += delta * block_speed
+		if block_length >= 100 or block_strength <= 0:
+			shield_sprite.visible = false
+			block_text.text = ""
+			block_label.text = ""
+			block_length = 0
+			block_strength = block_strength_save
+			state = States.IDLE
 	
 	#Attacking
 	if state == States.ATTACKING:
@@ -88,7 +124,14 @@ func on_unhover():
 func on_interact():
 	#Use Selected Card
 	if card_ui_node.card_instance_selected != null:
-		health -= card_ui_node.card_instance_selected.damage
+		if state == States.BLOCKING:
+			if block_strength - card_ui_node.card_instance_selected.damage >= 0:
+				block_strength -= card_ui_node.card_instance_selected.damage
+			else:
+				health -= card_ui_node.card_instance_selected.damage - block_strength
+				block_strength = 0
+		else:
+			health -= card_ui_node.card_instance_selected.damage
 		health += card_ui_node.card_instance_selected.health_regain
 		strength_buff += card_ui_node.card_instance_selected.strength_buff
 		element_water = card_ui_node.card_instance_selected.element_water
